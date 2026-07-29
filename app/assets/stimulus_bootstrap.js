6abe81782a38f5ef4db442cc5149300668826585
Bastien Le Caer <bastien@lecaer.net>
2026-07-29 23:04:44 +0200

    Transformation du sélecteur de denrée en recherche

--- app/assets/stimulus_bootstrap.js
+++ app/assets/stimulus_bootstrap.js
@@ -2,10 +2,12 @@ import { startStimulusApp } from '@symfony/stimulus-bundle';
 import FoodCatalogController from './controllers/food_catalog_controller.js';
 import FoodFormController from './controllers/food_form_controller.js';
 import SupplierDialogController from './controllers/supplier_dialog_controller.js';
+import SearchableSelectController from './controllers/searchable_select_controller.js';
 
 const app = startStimulusApp();
 app.register('food-catalog', FoodCatalogController);
 app.register('food-form', FoodFormController);
 app.register('supplier-dialog', SupplierDialogController);
+app.register('searchable-select', SearchableSelectController);
 // register any custom, 3rd party controllers here
 // app.register('some_controller_name', SomeImportedController);
