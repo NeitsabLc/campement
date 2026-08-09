a365ee33ceba66aea88d7dabeaef912b658c8dc3
Bastien Le Caer <bastien@lecaer.net>
2026-08-09 12:40:05 +0200

    Mise à jour documentation

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -10,6 +10,9 @@ install -d -o www-data -g www-data -m 0770 /var/www/app/var/documents_participan
 # changements. Un ancien répertoire compilé prendrait le dessus et figerait les
 # CSS/JS jusqu'à la prochaine compilation.
 if [ "${APP_ENV:-dev}" = "prod" ]; then
+    # Le répertoire compilé peut masquer les sources d'AssetMapper lors d'un
+    # déploiement suivant. Il est entièrement généré et doit repartir de zéro.
+    rm -rf public/assets
     php bin/console asset-map:compile --no-debug
 else
     rm -rf public/assets
